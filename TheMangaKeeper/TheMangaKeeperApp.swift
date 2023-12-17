8bba46734378f8ca3408a9c68617f1632ae4e7a0
gliadev <gliatem@gmail.com>
2023-12-17 21:51:45 +0100

    Initial Commit

---
+++ TheMangaKeeper/TheMangaKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMangaKeeperApp.swift
+//  TheMangaKeeper
+//
+//  Created by Adolfo on 17/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TheMangaKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
