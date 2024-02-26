f8b9d180a175e19345ae1872ecd27c6d832fbb5b
gliadev <gliatem@gmail.com>
2024-02-26 12:44:35 +0100

    vista grid en mi coleccion hecha.

--- TheMangaKeeper/MangasCollectionView.swift
+++ TheMangaKeeper/MangasCollectionView.swift
@@ -11,16 +11,21 @@ struct MangasCollectionView: View {
     @EnvironmentObject var mangasVM: MangasViewModel
     let manga: Manga
     
+    // defino la columnas estructura
+    let gridItems = [GridItem(.flexible()), GridItem(.flexible())]
+    
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(mangasVM.mangas.filter { $0.isFavorite }) { manga in
-                    NavigationLink(destination: MangasDetailView(manga: manga)){
-                        MangasCellView(manga: manga)
+            ScrollView {
+                LazyVGrid(columns: gridItems,spacing: 20) {
+                    ForEach(mangasVM.mangas.filter { $0.isFavorite }) { manga in
+                        NavigationLink(destination: MangasDetailView(manga: manga)){
+                            MangasCellView(manga: manga)
+                        }
                     }
                 }
             }
-            .navigationTitle("Mi Coleccion de Mangas")
+            .navigationTitle("Mi Coleccion")
             .bold()
         }
     }
