b6866019d4a933c1e77ad80a0f2b4f09b8339bad
gliadev <gliatem@gmail.com>
2024-02-26 13:14:07 +0100

    hay que cambiar la forma de llamar al grid

--- TheMangaKeeper/MangasCollectionView.swift
+++ TheMangaKeeper/MangasCollectionView.swift
@@ -11,13 +11,32 @@ struct MangasCollectionView: View {
     @EnvironmentObject var mangasVM: MangasViewModel
     let manga: Manga
     
-    // defino la columnas estructura
-    let gridItems = [GridItem(.flexible()), GridItem(.flexible())]
     
     var body: some View {
         NavigationStack {
-            ScrollView {
-                LazyVGrid(columns: gridItems,spacing: 20) {
+            List {
+                ForEach(mangasVM.mangas.filter { $0.isFavorite }) { manga in
+                    NavigationLink(destination: MangasDetailView(manga: manga)){
+                        MangasCellView(manga: manga)
+                    }
+                }
+            }
+            .navigationTitle("Mi Coleccion")
+            .bold()
+        }
+    }
+}
+    
+
+    #Preview {
+        MangasCollectionView(manga: .testManga)
+            .environmentObject(MangasViewModel.localTestMangas)
+    }
+
+        /*
+        NavigationStack {
+            //ScrollView {
+                //LazyVGrid(columns: gridItems,spacing: 20) {
                     ForEach(mangasVM.mangas.filter { $0.isFavorite }) { manga in
                         NavigationLink(destination: MangasDetailView(manga: manga)){
                             MangasCellView(manga: manga)
@@ -30,11 +49,7 @@ struct MangasCollectionView: View {
         }
     }
 }
-
-#Preview {
-    MangasCollectionView(manga: .testManga)
-        .environmentObject(MangasViewModel.localTestMangas)
-}
+*/
 
 
 
