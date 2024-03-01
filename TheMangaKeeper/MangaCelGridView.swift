b5e4a85a257bd4cd35dc94284600aab47c309606
gliadev <gliatem@gmail.com>
2024-03-01 12:42:45 +0100

    Poder elininar un manga desde tu coleccion

---
+++ TheMangaKeeper/MangaCelGridView.swift
@@ -0,0 +1,18 @@
+//
+//  MangaCelGridView.swift
+//  TheMangaKeeper
+//
+//  Created by Adolfo on 29/2/24.
+//
+
+import SwiftUI
+
+struct MangaCelGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MangaCelGridView()
+}
